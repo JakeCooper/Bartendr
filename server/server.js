06487e96bdf6e5adad58c84e88bbbc939cbac40f
Jake Cooper <jacob.cooper@hootsuite.com>
2015-10-17 22:30:56 -0700

    Added Drinks api @ /api?ingredient=jagermeister

---
+++ server/server.js
@@ -0,0 +1,25 @@
+/**
+ * Created by jacobc on 10/17/15.
+ */
+
+var app = require('express')(),
+    server = require('http').Server(app);
+
+server.listen(8080, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Locale webserver launched at http://%s:%s', host, port);
+});
+
+app.get('/api/', function (req, res) {
+    var ingredients = req.query.ingredient;
+    console.log("GET : Request received with ingredients : " + ingredients);
+    var drinks = DoSearch();
+    console.log("POST : Response sent with drinks : " + drinks);
+    res.send(drinks);
+});
+
+function DoSearch (ingredientList) {
+    return ["Jagerbombs"];
+}
